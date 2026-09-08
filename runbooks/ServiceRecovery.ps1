454b53f994508428be16382f3701f11d49451fb6
peter <157177875+petervancleave@users.noreply.github.com>
2026-09-08 11:20:45 -0600

    Update ServiceRecovery.ps1

--- runbooks/ServiceRecovery.ps1
+++ runbooks/ServiceRecovery.ps1
@@ -1,1 +1,13 @@
+Disable-AzContextAutosave -Scope Process
 
+$Context = (Connect-AzAccount -Identity).Context
+
+$Context = Set-AzContext `
+    -SubscriptionName $Context.Subscription `
+    -DefaultProfile $Context
+
+Write-Output "Authenticated successfully."
+
+Get-AzResourceGroup `
+    -Name "rg-itops-mvp" `
+    -DefaultProfile $Context
